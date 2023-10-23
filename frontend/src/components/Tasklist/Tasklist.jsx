650e2c0df339109fda1e4fdef758cbe12762e9e0
rvijaykr10 <rvijaykr10@gmail.com>
2023-10-23 15:42:50 +0530

    post API integration done

--- frontend/src/components/Tasklist/Tasklist.jsx
+++ frontend/src/components/Tasklist/Tasklist.jsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import styles from "./Tasklist.scss";
 
 const Tasklist = () => {
   const [tasks, setTasks] = useState([]);
+
+  const inputRef = useRef();
 
   const getTasks = async () => {
     const result = await axios("/api/tasks");
@@ -20,42 +22,52 @@ const Tasklist = () => {
     getTasks();
   }, []);
 
-  if (tasks?.length === 0) {
-    return (
-      <div className={styles.tasklistContainer}>
-        <h3>No tasks to show</h3>
-      </div>
-    );
-  }
+  const addTask = async () => {
+    if (inputRef?.current.value === "") return;
+    await axios.post("/api/tasks", { task: inputRef.current.value });
+    inputRef.current.value = "";
+    getTasks();
+  };
 
   return (
     <div className={styles.tasklistContainer}>
-      <div className={styles.list}>
-        <ul>
-          <li>
-            <div>#</div>
-            <div>TASK</div>
-            <div>
-              {/* <span>EDIT</span> */}
-              <span>DELETE</span>
-            </div>
-          </li>
-          {tasks?.map((obj, index) => (
-            <li key={obj?._id}>
-              <div>{index + 1}</div>
-              <div>{obj?.task}</div>
+      <div className={styles.textButtonContainer}>
+        <input type="text" name="task" id="task" ref={inputRef} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      {tasks?.length === 0 && (
+        <div className={styles.noTasks}>
+          <h3>No tasks to show</h3>
+        </div>
+      )}
+      {tasks?.length > 0 && (
+        <div className={styles.list}>
+          <ul>
+            <li>
+              <div>#</div>
+              <div>TASK</div>
               <div>
-                {/* <span>
-                <button>edit</button>
-              </span> */}
-                <span>
-                  <button onClick={() => deleteTask(obj?._id)}>delete</button>
-                </span>
+                {/* <span>EDIT</span> */}
+                <span>DELETE</span>
               </div>
             </li>
-          ))}
-        </ul>
-      </div>
+            {tasks?.map((obj, index) => (
+              <li key={obj?._id}>
+                <div>{index + 1}</div>
+                <div>{obj?.task}</div>
+                <div>
+                  {/* <span>
+                <button>edit</button>
+              </span> */}
+                  <span>
+                    <button onClick={() => deleteTask(obj?._id)}>delete</button>
+                  </span>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
