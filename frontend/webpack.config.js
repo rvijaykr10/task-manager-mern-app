872b60c78c593967c87b605ae40b18883ad874fa
rvijaykr10 <rvijaykr10@gmail.com>
2023-12-16 14:58:48 +0530

    webpack files changed

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,23 +1,34 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "build"),
     clean: true,
     assetModuleFilename: "images/[hash][ext][query]",
     publicPath: "/",
   },
   devtool: "inline-source-map",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    minimizer: [new TerserPlugin()],
+  },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
     }),
     new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new CompressionPlugin(),
   ],
   module: {
     rules: [
@@ -47,8 +58,13 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        type: "asset/resource",
+        test: /\.(png|jpg|gif|svg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 10kb
+          },
+        },
       },
     ],
   },
--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,21 +1,32 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
-    assetModuleFilename: "images/[hash][ext][query]",
+    assetModuleFilename: "assets/[hash][ext][query]",
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+    minimizer: [new TerserPlugin()],
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
     }),
     new MiniCssExtractPlugin(),
+    new CleanWebpackPlugin(),
+    new CompressionPlugin(),
   ],
   module: {
     rules: [
@@ -32,7 +43,6 @@ module.exports = {
       {
         test: /\.css$/,
         use: [
-          //  "style-loader","css-loader"
           { loader: MiniCssExtractPlugin.loader },
           { loader: "css-loader", options: { modules: true } },
         ],
@@ -46,8 +56,13 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|jpg|gif)$/,
-        type: "asset/resource",
+        test: /\.(png|jpg|gif|svg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // 10kb
+          },
+        },
       },
     ],
   },
